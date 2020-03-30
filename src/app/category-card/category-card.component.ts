0a28bcfb1d7741d9c44e53c08ab577809b6bc5f5
Toby Ladislas <tladislas@insiten.com>
2020-03-30 19:01:34 -0400

    Add active button styling, conditional to search bar -- category must be selected to show search.

--- src/app/category-card/category-card.component.ts
+++ src/app/category-card/category-card.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CategoryCardComponent implements OnInit {
 	@Input() categoryName: string;
+	@Input() isActive: string;
 
 	constructor() { }
 
