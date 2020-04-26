16e815e6af36a5135b4232a2163c10d76891529c
Toby Ladislas <tladislas@gmail.com>
2020-04-26 19:12:59 -0400

    Refactor class variables that can be updated into a behavior subject.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+interface ISearch {
+	activeCategory: string;
+	searchValue: string;
+	results: any[];
+}
 
 @Component({
 	selector: 'search',
@@ -6,9 +13,12 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-	public activeCategory: string;
-	public searchValue: string;
-	public results: any[];
+	public defaultValues = {
+		activeCategory: "",
+		searchValue: "",
+		results: []
+	}
+	public state$: BehaviorSubject<ISearch> = new BehaviorSubject<ISearch>(this.defaultValues);
 	public noMatches: boolean;
 	public categories: string[] = [
 		"Synonyms",
@@ -17,34 +27,40 @@ export class SearchComponent implements OnInit {
 		"Homophones"
 	];
 
+
 	constructor() { }
 
 	ngOnInit(): void {
+		this.addSearchOnEnter();
 	}
 
 	public setCategory(category): void {
-		this.activeCategory = category;
-		this.results = [];
-		this.searchValue = "";
+		this.set("activeCategory", category);
+		this.set("searchValue", "");
+		this.set("results", []);
 		this.noMatches = false;
 	}
 
 	public setSearchText($event): void {
 		this.noMatches = false;
-		this.searchValue = $event.target.value;
+		this.set("searchValue", $event.target.value);
 	}
 
 	public async search(): Promise<void> {
-		if (this.activeCategory && this.searchValue) {
+		if (this.isSearchReady) {
 			const queryString = this.setQuery();
 			const datamuseAPIResults = await fetch(`https://api.datamuse.com/words?rel_${queryString}`);
 			const resultsJSON = await datamuseAPIResults.json();
-			this.results = resultsJSON;
+			this.set("results", resultsJSON);
 
-			if (!this.results.length) {
+			if (!this.get("results").length) {
 				this.noMatches = true;
 			}
 		}
+	}
+
+	private isSearchReady() {
+		return this.get("activeCategory") && this.get("searchValue");
 	}
 
 	private setQuery(): string {
@@ -55,7 +71,25 @@ export class SearchComponent implements OnInit {
 			"Homophones": "hom"
 		}
 
-		return categoryFetchMap[this.activeCategory] + "=" + this.searchValue;
+		return categoryFetchMap[this.get("activeCategory")] + "=" + this.get("searchValue");
+	}
+
+	private addSearchOnEnter() {
+		document.addEventListener("keyup", ($e) => {
+			if (this.get("activeCategory") && this.get("searchValue") && $e.keyCode === 13) {
+				this.search();
+			}
+		});
+	}
+
+	private get(property) {
+		return this.state$.value[property];
+	}
+
+	private set(property, value) {
+		const currentState = this.state$.value;
+		const newState = { ...currentState, [property]: value };
+		this.state$.next(newState);
 	}
 
 
